1a2d386061c13a72cad572711806a74b81b4beda
marcellinus-witarsah <aw.marcel@gmail.com>
2022-08-18 21:59:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def dice_coef(y_true, y_pred):
 
 
 # load model
-model_path = "models/landsat_8/unet_model_opt_adam_lr_0.001_batch_32_epochs_20_filters_32_size_144_date_20220809"
+model_path = "models/sentinel_2/unet_model_opt_adam_lr_0.001_batch_32_epochs_20_filters_16_size_176_date_20220810"
 unet_model = tf.keras.models.load_model(model_path, custom_objects={'dice_coef': dice_coef})
 
 # Returns pretty much every information about your model
@@ -33,14 +33,22 @@ uploaded_file = st.file_uploader("Please Input Wildfire Image", type="png")
 if uploaded_file:
     # read file as byte
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+
     # decode file
     image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+
     # convert image to RGB
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     # prepare image for display
-    image_for_display = cv2.resize(image, (500, 500))
+    scale_percentage = 0.6
+    scaled_height = int(image.shape[0]*scale_percentage)
+    scaled_width = int(image.shape[1]*scale_percentage)
+    image_for_display = cv2.resize(image, (scaled_height, scaled_width))
+
     # display image
     st.image(image_for_display * 3, channels="RGB", caption="Uploaded file")
+
     # prepare image for input
     resized_image = cv2.resize(image, (height, width))
     normalized_image = tf.cast(resized_image / 255, tf.dtypes.float32)
@@ -48,15 +56,19 @@ if uploaded_file:
 
     # Predict Button
     generate_prediction = st.button("Generate Prediction")
+
     # Prediction Result
     if generate_prediction:
         result = unet_model.predict(input_data)
+
         # apply threshold to the result prediction
         np_mask = np.array(result[0])
         np_mask = np.where(np_mask < 0.5, 0, 1)
         np_mask = np_mask * 255
+        np_mask = np_mask.astype(np.uint8)
+
         # prepare mask for display
-        mask_for_display = np.resize(np_mask, (500, 500))
+        mask_for_display = cv2.resize(np_mask, (scaled_height, scaled_width), interpolation=cv2.INTER_AREA)
         st.image(mask_for_display, caption="Prediction Result")
 
 
